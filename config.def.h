5b51e7b20071d50f312fcc9f95acdfd13169bea4
Andreas Hauser <andreas.hauser@vde.com>
2021-03-08 10:30:24 +0800

    Updated keycodes

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /*
  * Define ESC sequences to use for scroll events.
- * Use "cat -v" to figure out favorite key combination.
+ * Use "showkey -a" to figure out favorite key combination.
  *
  * lines is the number of lines scrolled up or down.
  * If lines is negative, it's the fraction of the terminal size.
@@ -8,14 +8,16 @@
 
 struct rule rules[] = {
 	/* sequence     event        lines */
-	//{"\033[5;2~",   SCROLL_UP,   -1},	/* [Shift] + [PageUP] */
-	//{"\033[6;2~",   SCROLL_DOWN, -1},	/* [Shift] + [PageDown] */
+	{"\033[5;2~",   SCROLL_UP,   -1},	/* [Shift] + [PageUP] */
+	{"\033[6;2~",   SCROLL_DOWN, -1},	/* [Shift] + [PageDown] */
 	/* mouse binding shadows ^E and ^Y, so it's disabled by default */
-	{"\031",        SCROLL_UP,    1},	/* mouse wheel up */
-	{"\005",        SCROLL_DOWN,  1},	/* mouse wheel Down */
-	/* additional binsings */
-	//{"k",           SCROLL_UP,    1},	/* mouse wheel up */
-	//{"j",           SCROLL_DOWN,  1},	/* mouse wheel Down */
-	//{"u",           SCROLL_UP,    20},	/* mouse wheel up */
-	//{"d",           SCROLL_DOWN,  20},	/* mouse wheel Down */
+	//{"\031",        SCROLL_UP,    1},	/* mouse wheel up */
+	//{"\005",        SCROLL_DOWN,  1},	/* mouse wheel Down */
+	{"\033\125",    SCROLL_UP,    1},	/* mouse wheel up */
+	{"\033\104",    SCROLL_DOWN,  1},	/* mouse wheel Down */
+	/* additional bindings */
+	{"\033\153",    SCROLL_UP,    1},	/* Alt + k */
+	{"\033\152",    SCROLL_DOWN,  1},	/* Alt + j */
+	{"\033\165",    SCROLL_UP,    20},	/* Alt + u */
+	{"\033\144",    SCROLL_DOWN,  20},	/* Alt + d */
 };
